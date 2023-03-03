9edf476e03baaea068f8f2e6eefe96f3fa8b8862
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-03 17:13:18 -0300

    feat: add controller

--- src/routes/LoginRouter.js
+++ src/routes/LoginRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/');
+const LoginController = require('../controllers/LoginController');
+
+router.post('/', LoginController.login);
 
 module.exports = router;
