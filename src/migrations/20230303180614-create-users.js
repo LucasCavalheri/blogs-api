f6ab537fd6490dc6799e9c68fa2ae991919df893
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-03 15:12:46 -0300

    feat: add create-user Migration

---
+++ src/migrations/20230303180614-create-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Query } = require('mysql2/lib/commands');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
