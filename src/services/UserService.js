ae42d58a3286fb7f8ded361a8c43143ffa16354b
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-03 18:37:47 -0300

    feat: add createUser function on UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,7 +4,10 @@ const searchByPassword = (password) => User.findOne({ where: { password } });
 
 const searchByEmail = (email) => User.findOne({ where: { email } });
 
+const createUser = (userData) => User.create(userData);
+
 module.exports = {
   searchByPassword,
   searchByEmail,
+  createUser,
 };
