ee1a078f6ca8d19269a1b9cd9da784a17e342507
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 19:37:54 -0300

    feat: add updatePost function on BlogPostRouter and add req15

--- src/routes/BlogPostRouter.js
+++ src/routes/BlogPostRouter.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/', validateToken, BlogPostController.getAllPostsWithUserAndCategory);
 router.get('/:id', validateToken, BlogPostController.getUniquePostWithUserAndCategory);
 router.post('/', validateToken, BlogPostController.createBlogPost);
+router.put('/:id', validateToken, BlogPostController.updatePost);
 
 module.exports = router;
