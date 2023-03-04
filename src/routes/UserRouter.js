e25929fcd6d749dcc216315b8ee5dfb577c25a7f
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-03 21:59:32 -0300

    feat: add validateToken middleware and getAllUsers route

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { validateCreateUser } = require('../middlewares/validateCreateUser');
+const { validateToken } = require('../middlewares/validateToken');
 
 const UserController = require('../controllers/UserController');
 
 const router = express.Router();
 
+router.get('/', validateToken, UserController.getAllUsers);
 router.post('/', validateCreateUser, UserController.createUser);
 
 module.exports = router;
