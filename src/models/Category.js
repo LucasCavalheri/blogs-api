f3e9f039869b9bbe3f6b13e3af7e2ece78491756
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 09:48:03 -0300

    feat: add Category Model and add req07

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ * @returns
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      name: DataTypes.STRING,
+    },
+    {
+      tableName: 'categories',
+      timestamps: false,
+      underscored: true,
+    },
+  );
+
+  return Category;
+};
