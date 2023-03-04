77b98265a2ac8499e13bb7f80b11e2ae825cdaec
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 17:35:15 -0300

    feat: add createBlogPost function on BlogPostController and add req12

---
+++ src/controllers/BlogPostController.js
@@ -0,0 +1,26 @@
+const BlogPostService = require('../services/BlogPostService');
+const CategoryService = require('../services/CategoryService');
+
+const createBlogPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const userId = req.payload.payload.id;
+
+  if (!title || !content || !categoryIds) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+
+  const categoryIdsExists = await CategoryService.searchCategoriesById(categoryIds);
+
+  if (categoryIdsExists.includes(null)) {
+    return res.status(400).json({ message: 'one or more "categoryIds" not found' });
+  }
+
+  const blogPostCreated = await BlogPostService
+    .createBlogPost({ title, content, userId, categoryIds });
+
+  return res.status(201).json(blogPostCreated);
+};
+
+module.exports = {
+  createBlogPost,
+};
