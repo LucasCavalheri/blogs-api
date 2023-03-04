b483730dff1702c96a12c4217e27ce76e2682bd7
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 10:05:19 -0300

    feat: add CategoryRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const LoginRouter = require('./LoginRouter');
 const UserRouter = require('./UserRouter');
+const CategoryRouter = require('./CategoryRouter');
 
 const router = express.Router();
 
 router.use('/login', LoginRouter);
 router.use('/user', UserRouter);
+router.use('/categories', CategoryRouter);
 
 module.exports = router;
