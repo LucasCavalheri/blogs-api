2deb6d13db305254b5678edc3bdd5bea7c20a5a3
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 17:46:49 -0300

    feat: add getAllPostsWithUserAndCategory function on BlogPostRouter and add req13

--- src/routes/BlogPostRouter.js
+++ src/routes/BlogPostRouter.js
@@ -5,6 +5,7 @@ const { validateToken } = require('../middlewares/validateToken');
 
 const router = express.Router();
 
+router.get('/', validateToken, BlogPostController.getAllPostsWithUserAndCategory);
 router.post('/', validateToken, BlogPostController.createBlogPost);
 
 module.exports = router;
