8bf063f03ea79154f37a559213e43597d8b0bf4d
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 12:41:33 -0300

    feat: add getAllCategories function on CategoryService

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,7 +1,10 @@
 const { Category } = require('../models');
 
+const getAllCategories = async () => Category.findAll();
+
 const createCategory = async (categoryData) => Category.create(categoryData);
 
 module.exports = {
+  getAllCategories,
   createCategory,
 };
