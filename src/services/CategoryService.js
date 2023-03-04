3ba0f700bd42080fe212181e66edc1a5c8f0e837
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-04 17:34:03 -0300

    feat add searchCategoriesById function on CategoryService

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -2,9 +2,19 @@ const { Category } = require('../models');
 
 const getAllCategories = async () => Category.findAll();
 
+const searchCategoriesById = async (categoriesId) => {
+  const categoriesIdExists = await categoriesId.map((category) =>
+    Category.findOne({ where: { id: category } }));
+    
+  const exists = await Promise.all(categoriesIdExists);
+
+  return exists;
+};
+
 const createCategory = async (categoryData) => Category.create(categoryData);
 
 module.exports = {
   getAllCategories,
+  searchCategoriesById,
   createCategory,
 };
